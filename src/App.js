4b3e8807419764501c3a9ef357b81bb7bfff785e
Martin Koch <martin_emanuel.koch@smail.th-koeln.de>
2020-05-02 00:11:07 +0200

    Bugfix in css layout and react wireframe

--- src/App.js
+++ src/App.js
@@ -1,48 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { correct, improveSolution } from './solver.js'
+import {correct, improveSolution} from './solver.js'
 
-function Stepper(props){
-  return(
-  <button className="step" onClick={props.onClick}>Step
-  </button>)
+function Stepper(props) {
+    return (<button className="step"
+        onClick={
+            props.onClick
+    }>Hint
+    </button>)
 }
 
-function Solver(props){
-  return(
-  <button className="solve" onClick={props.onClick}>Solve
-  </button>)
+function Solver(props) {
+    return (<button className="solve"
+        onClick={
+            props.onClick
+    }>Init
+    </button>)
 }
 
 function Square(props) {
     if (props.value.constructor === Number) {
-        return (<button className={"square"}
-            onClick={
-                props.onClick
-        }> {
-            props.value
-        } </button>);
-    } else {
+        return (<td className="container"><div onClick={props.onClick} className={"square"}><span>{props.value}</span></div></td>);
+    } 
+    else {
         let values = [];
         for (let i = 0; i < 3; i++) {
             let row = [];
             for (let j = 0; j < 3; j++) {
                 const id = i * 3 + j + 1;
                 const show = props.value.indexOf(id) > -1 ? "possible" : "impossible";
-                const el = <div key={id}
-                    className={show}> {id}</div>;
+                const el = <div key={id} className={show}>{id}</div>;
                 row.push(el);
             }
-            const divRow = <div key={i}
-                className="possible-row"> {row}</div>;
-            values.push(divRow);
+            values.push(row);
         }
-
-        return (<button className={"multi_square"}
-            onClick={
-                props.onClick
-        }> {values} </button>);
+        return (<td className="container"><div onClick={props.onClick} className={"multi_square"}>{values}</div></td>);
+   
     }
 }
 
@@ -53,7 +47,8 @@ class Board extends React.Component {
             value={
                 this.props.squares[i]
             }
-            //onClick={() => this.props.onClick(i)}
+            index={i}
+            onClick={()=>{alert("ah");}}//this.props.onClick(i)}
         />);
     }
 
@@ -62,14 +57,20 @@ class Board extends React.Component {
         for (let j = 0; j < 9; j++) {
             let row = [];
             for (let i = 0; i < 9; i++) {
-                const square = this.renderSquare(j * 9 + i);
+                const index = j * 9 + i;
+                const square = this.renderSquare(index);
+                /*if (index % 3 === 0 && index % 9 !== 0) 
+                    row.push (<div key={"bound"+index} className="vertical-bounds"></div>);*/
                 row.push(square);
+                
             }
-            const divRow = <div key={j}
-                className="board-row"> {row}</div>;
+            const divRow = <tr key={j} className="board-row">{row}</tr>;
+            /*if(j%3===0 && j !==0)
+                squares.push(<div key={"bound"+j} className="horizontal-bounds"></div>);*/
             squares.push(divRow);
+
         }
-        return (<div> {squares}</div>);
+        return (<tbody>{squares}</tbody>);
     }
 }
 
@@ -78,31 +79,37 @@ class Sudoku extends React.Component {
         super(props);
         this.state = {
             squares: correct,
-            initialized:false
+            initialized: false
         }
     };
-    
-    step = ()=>{
-      const old = this.state.squares;
-      const improved = improveSolution(old);
-      this.setState({squares: improved});
+
+    step = () => {
+        const old = this.state.squares;
+        const improved = improveSolution(old);
+        this.setState({squares: improved});
 
     };
 
-    initialize=()=>{
-      this.setState({squares: this.state.squares.map(el => el ? el : [...Array(10).keys()].splice(1)) ,initialized:true});
+    initialize = () => {
+        this.setState({
+            squares: this.state.squares.map(el => el ? el : [...Array(10).keys()].splice(1)),
+            initialized: true
+        });
     }
 
     render() {
-        const navigation = this.state.initialized?<Stepper onClick={this.step}/>:<Solver onClick={this.initialize}/>
+        const navigation = this.state.initialized ? <Stepper onClick={
+            this.step
+        }/> : <Solver onClick={
+            this.initialize
+        }/>
         return (<div className="game">
-            <div className="game-board">
+            <table className="game-board">
                 <Board squares={
                     this.state.squares
                 }/>
-            </div>
-            {navigation}
-        </div>);
+            </table>
+            {navigation} </div>);
     }
 }
 
--- src/solver.js
+++ src/solver.js
@@ -9,8 +9,8 @@ const correct = [
        7, 0, 3, 4, 0, 0, 5, 6, 0 ,
        0, 0, 0, 0, 0, 0, 0, 0, 0 ,
   ];
-
-const worldHardestsudoku  = [
+/*
+const correct  = [
      8, 0, 0, 0, 0, 0, 0, 0, 0,
      0, 0, 3, 6, 0, 0, 0, 0, 0,
      0, 7, 0, 0, 9, 0, 2, 0, 0 ,
@@ -20,141 +20,103 @@ const worldHardestsudoku  = [
      0, 0, 1, 0, 0, 0, 0, 6, 8 ,
      0, 0, 8, 5, 0, 0, 0, 1, 0 ,
      0, 9, 0, 0, 0, 0, 4, 0, 0 ,
-];
+];*/
+
 function solve9x9(sudoku) {
-    let iteration = 0;
-    // fill null with Range(1,9)
-    sudoku = sudoku.map(el => el ? el : [...Array(10).keys()].splice(1));
+let iteration = 0;
+// fill null with Range(1,9)
+sudoku = sudoku.map(el => el ? el : [...Array(10).keys()].splice(1));
 
-    while (iteration++ < 500) {
-        console.log(sudoku.reduce((accumulator, currentValue) => (currentValue.constructor === Number) ? accumulator + 1 : accumulator, 0));
+while (iteration++ < 500) {
+    console.log(sudoku.reduce((accumulator, currentValue) => (currentValue.constructor === Number) ? accumulator + 1 : accumulator, 0));
 
-        const old = Object.assign({}, sudoku);
+    const old = Object.assign({}, sudoku);
 
-        sudoku = improveSolution(sudoku);
+    sudoku = improveSolution(sudoku);
 
-        // all numbers found
-        if (sudoku.reduce((accumulator, currentValue) => accumulator && (currentValue.constructor === Number))) 
-            return sudoku;
-        
+    // all numbers found
+    if (sudoku.reduce((accumulator, currentValue) => accumulator && (currentValue.constructor === Number))) 
+        return sudoku;
+    
 
-        // check if progress has been made this turn otherwise return error in sudoku
-        if (JSON.stringify(sudoku) === JSON.stringify(old)) 
-            throw "sudoku  is errorsome.";
-        
 
-    }
+    // check if progress has been made this turn otherwise return error in sudoku
+    if (JSON.stringify(sudoku) === JSON.stringify(old)) 
+        throw "sudoku  is errorsome.";
 
-};
+}};
 
 function improveSolution(sudoku) {
-    // iterate through blocks of 3x3 -> right then down
-    sudoku = Object.assign({}, sudoku);
-
     const crossout = indices => {
         let numbers = [],
             arrayIndices = [];
-        indices.foreach(index => {
-            if (sudoku[index].constructor === Number) 
-                numbers.push(sudoku[index]);
-             else 
-                arrayIndices.push(index);
+        indices.forEach(index => {
+            if (sudoku[index].constructor == Number){
+                numbers.push(sudoku[index]);}
+             else{
+                arrayIndices.push(index);}
             
             for (const arrayIndex of arrayIndices) {
-                let newGuess = sudoku[arrayIndex]. filter(el => numbers.indexOf(el) < 0);
+                console.log(arrayIndex,sudoku[arrayIndex]);
+                let newGuess = sudoku[arrayIndex].filter(el => numbers.indexOf(el) < 0);
                 if (newGuess.length === 1) {
                     newGuess = newGuess[0];
                     numbers.push(newGuess);
+                    arrayIndices = arrayIndices.filter(index=> index !== arrayIndex);
                 }
-                if (newGuess.length <= 0) 
-                    throw "Error at index " + arrayIndex;
+                else if (newGuess.length <= 0){ 
+                    throw "Error at index " + arrayIndex;}
                 
                 sudoku[arrayIndex] = newGuess;
             }
         });
     };
+
+    sudoku = Object.assign({}, sudoku);
+
     for (let row = 0; row < 3; row++) {
         for (let col = 0; col < 3; col++) {
-            debugger;
-            let numbers = [],
-                arrayIndices = [];
-            // check square
+
+            let checksquare = [];
             for (let j = 0; j < 3; j++) {
                 for (let i = 0; i < 3; i++) {
                     const index = (row * 3 + j) * 9 + col * 3 + i;
-
-                    if (sudoku[index].constructor === Number) 
-                        numbers.push(sudoku[index]);
-                     else 
-                        arrayIndices.push(index);
-                    
-                    for (const arrayIndex of arrayIndices) {
-                        let newGuess = sudoku[arrayIndex] ?. filter(el => numbers.indexOf(el) < 0);
-                        if (newGuess.length === 1) {
-                            newGuess = newGuess[0];
-                            numbers.push(newGuess);
-                        }
-                        if (newGuess.length<= 0)
-                            throw "Error at index "+arrayIndex;
-                        sudoku [arrayIndex] = newGuess;
-                    }
+                    checksquare.push(index);
                 }
             }
-            // if end of col -> check col if (col === 2) {
-                            for (let i = 0; i < 3; i++) {
-                                numbers = [];
-                                arrayIndices = [];
-                                for (let j = 0; j < 9; j++) {
-                                    let index = (row * 3 + i) * 9 + j; // TODO
-                                    if (sudoku[index].constructor === Number) 
-                                        numbers.push(sudoku[index])
-                                     else 
-                                        arrayIndices.push(index);
-                                    
-                                }
-                                for (const arrayIndex of arrayIndices) {
-                                    let newGuess = sudoku[arrayIndex] ?. filter(el => numbers.indexOf(el) < 0);
-                                    if (newGuess.length === 1) {
-                                        newGuess = newGuess[0];
-                                        numbers.push(newGuess);
-                                    }
-                                    sudoku[arrayIndex] = newGuess;
-                                }
-                            }
-                        }
-                        // if end of row -> check row
-                            if (row === 2) {
-                            for (let i = 0; i < 3; i++) {
-                                numbers = [];
-                                arrayIndices = [];
-                                for (let j = 0; j < 9; j++) {
-                                    let index = j * 9 + col * 3 + i; // TODO
-                                    if (sudoku[index].constructor === Number) 
-                                        numbers.push(sudoku[index])
-                                     else 
-                                        arrayIndices.push(index);
-                                    
+            crossout(checksquare);
 
-                                }
-                                for (const arrayIndex of arrayIndices) {
-                                    let newGuess = sudoku[arrayIndex] ?. filter(el => numbers.indexOf(el) < 0);
-                                    if (newGuess.length === 1) {
-                                        newGuess = newGuess[0];
-                                        numbers.push(newGuess);
-                                    }
-                                    sudoku[arrayIndex] = newGuess;
-                                }
-                            }
-                        }
+            if (col === 2) {
+                for (let i = 0; i < 3; i++) {
+                    let checkcol = [];
+                    for (let j = 0; j < 9; j++) {
+                        const index = (row * 3 + i) * 9 + j;
+                        checkcol.push(index);
                     }
+                    crossout(checkcol);
                 }
-
-                return sudoku;
+                
             }
+            if (row === 2) {
+                for (let i = 0; i < 3; i++) {
+                    let checkrow = [];
+                    for (let j = 0; j < 9; j++) {
+                        const index = j * 9 + col * 3 + i;
+                        checkrow.push(index);
+                    }
+                    crossout(checkrow);
 
-            export {
-                solve9x9 as solve9x9,
-                improveSolution as improveSolution,
-                correct as correct
-            };
+                }
+            }
+        }
+    }
+
+    return sudoku;
+}
+
+export {
+    solve9x9 as solve9x9,
+    improveSolution as improveSolution,
+    correct as correct
+};
 
