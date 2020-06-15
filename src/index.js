b30d8c9524a3da40603e5f1fa8b50947351de51d
Martin Koch <martin_emanuel.koch@smail.th-koeln.de>
2020-06-15 09:55:28 +0200

    Added cmd/shift/alt shortcut to input notes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Sudoku from './components/sudoku';
+import Sudoku from './components/Sudoku';
 import * as serviceWorker from './serviceWorker';
-import {solved,start} from './logic/solver.js'
+import {solved, start} from './logic/solver.js'
 
-ReactDOM.render(
-    <Sudoku start={start} solved={solved}/>
-,  document.getElementById('root')
-);
+function Game(props) {
+    const [restart, setRestart] = useState(false);
+    return (<Sudoku sudoku={
+            {
+                start: start,
+                solved: solved,
+                difficulty: "easy"
+            }
+        }
+        restart={restart}
+        setRestart={setRestart}/>);
+}
+
+ReactDOM.render (<Game/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
