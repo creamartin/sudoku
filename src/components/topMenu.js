f4da0d3d72f71b3f9d621c0040db2761d7afc5f0
Martin Koch <martin42koch@gmail.com>
2020-06-19 14:19:37 +0200

    fix naming error

--- src/components/topMenu.js
+++ src/components/topMenu.js
@@ -1,9 +1,7 @@
-import React, {useState, useEffect, useCallback} from "react";
+import React from "react";
 import './topmenu.css';
 import Menu from './menu.js';
-import Dropdown from './dropdown.js';
 import Timer from './timer.js';
-import ToggleSwitch from './toggleSwitch.js';
 
 export default function TopMenu(props) {
     return (<div className="topmenu_wrapper">
